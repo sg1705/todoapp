e7b79a4f6167bb925543b0b9b49cf62d27927333
Shobhit Gupta <shobhitg@vmware.com>
2016-06-22 09:31:11 -0700

    Single file onle

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,10 @@
-var Todo = require('todo');
+class Todo {
+    constructor(name, assignee, creator) {
+        this.name = name;
+        this.assignee = assignee;
+        this.creator = creator;
+    }
+}
 
 console.log('\'Allo \'Allo!');
 // var newTodo = new Todo('first task', 'system');
--- app/scripts/todo.js
+++
@@ -1,7 +0,0 @@
-class Todo {
-    constructor(name, assignee, creator) {
-        this.name = name;
-        this.assignee = assignee;
-        this.creator = creator;
-    }
-}
